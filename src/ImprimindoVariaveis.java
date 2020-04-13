3e9666f17238f34109d4493996a8ec4a864fdf03
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-04-13 20:09:21 -0300

    Aula 5 - Tp primitivos, convenções de código e comentários

---
+++ src/ImprimindoVariaveis.java
@@ -0,0 +1,7 @@
+public class ImprimindoVariaveis {
+    public static void main(String[] args) {
+        int idade = 36;
+        System.out.println("Dados impressos");
+        System.out.println("Minha idade é = " + idade + " anos");
+    }
+}
--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -1,5 +1,7 @@
 public class OlaMundo {
     public static void main(String[] args) {
+        System.out.println("------------");
         System.out.println("Ola mundo");
+        System.out.println("------------");
     }
 }
